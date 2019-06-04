5b7b3a964545f7c4bb7fc3fe78e9e34156e1fc7b
Pankaj Shinde2 <PS00475391@TechMahindra.com>
2019-06-04 18:44:04 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Greet name="bruce"/> 
+    <Welcome name="bruce wayne">
+    <p>This Is children</p>
+    </Welcome>
+    <Hello />
     </div>
   );
 }
